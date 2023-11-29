5ddb112f64bc6ce348e1a55ad46a067d7b81d125
stndc <0.stndc@gmail.com>
2023-11-29 17:08:28 -0300

    cambios en espacio tres

--- phpfiles/espacio_dos.php
+++ phpfiles/espacio_dos.php
@@ -1,37 +1,20 @@
 
 <div class="container py-5">
+<h5 class="mb-3">Reservas - <a href="./">Home</a></h5>
+
 <?php
-
-function connect() {
-    $host = 'localhost';
-    $db = 'reservas';
-    $usuario = 'root';
-    $contrasena = '';
-
-    try {
-        $pdo = new PDO("mysql:host=$host;dbname=$db", $usuario, $contrasena);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        echo "Error de conexión: " . $e->getMessage();
-        die();
-    }
-}
-
-$pdo = connect();
 
 $id = 1; // el id debe ser el del usuario logueado
 
 $disponible = range(11, 16);
-
 $izquierda = array_slice($disponible, 0, 3);
 $derecha = array_slice($disponible, 3);
 
 $fecha = $_GET['fecha'];
 
 $ass_dis = $app->habilitados($fecha, $disponible);
+$reservas = $app->rango($fecha, $disponible);
 
-$reservas = rango($fecha, $disponible, $pdo);
 ?>
 
 <div class="row">
@@ -39,44 +22,21 @@ $reservas = rango($fecha, $disponible, $pdo);
 <div class="shadow rounded-4 p-3">
 <h5 class="mb-3">Reservados para el d&iacute;a <?=$fecha;?></h5>
 
-
 <?php
+
 if (empty($reservas)) {
-    echo '<p class="mb-0">No hay lugares reservados para esta fecha.</p>';
+	echo '<p class="mb-0">No hay lugares reservados para esta fecha.</p>';
 } else {
-    foreach ($reservas as $reserva) {
+	foreach ($reservas as $reserva) {
 ?>
-<div class="d-flex justify-content-between align-items-center">
-<p class="text-capitalize mb-0"><?=$reserva['nombre'];?></p>
-Asiento #<?=$reserva['asiento'];?>
-</div>
-<hr>
+		<div class="d-flex justify-content-between align-items-center">
+			<p class="text-capitalize mb-0"><?=$reserva['nombre'];?></p>
+			Asiento #<?=$reserva['asiento'];?>
+		</div>
+		<hr>
 <?php
-    }
+	}
 }
-
-function rango($fecha, $disponible, $pdo){
-
-    $sql = "SELECT * FROM lista LEFT JOIN usuarios ON lista.idusuario = usuarios.id WHERE lista.fecha = :fecha AND lista.asiento IN (" . implode(',', $disponible) . ")";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':fecha', $fecha, PDO::PARAM_STR);
-    $stmt->execute();
-
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-
-function reservas_hoy($fecha, $pdo){
-
-    $sql = "SELECT * FROM lista LEFT JOIN usuarios ON lista.idusuario = usuarios.id WHERE lista.fecha = :fecha";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':fecha', $fecha, PDO::PARAM_STR);
-    $stmt->execute();
-
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-
-
-
 
 $ass_dis = $app->habilitados($fecha, $disponible);
 
@@ -91,42 +51,39 @@ $ass_dis = $app->habilitados($fecha, $disponible);
 <h5 class="mb-3">Seleccione un asiento</h5>
 
 <form action="" method="post">
-
 <div class="tablet">
 <div class="left-chairs">
-<?php foreach ($izquierda as $asiento) { ?>
-<label class="chair" style="border-radius: 10px 0 0 10px;">
 <?php
-$disabled = in_array($asiento, $ass_dis) ? '' : 'disabled';
+foreach ($izquierda as $asiento) {
 ?>
-<input type='radio' name='asiento' value='<?=$asiento;?>' <?=$disabled;?>> 
-<img src="assets/img/office-chair-left.png">
-
-
-</label>
-<?php } ?>
+	<label class="chair" style="border-radius: 10px 0 0 10px;">
+<?php
+		$disabled = in_array($asiento, $ass_dis) ? '' : 'disabled';
+?>
+		<input type='radio' name='asiento' value='<?=$asiento;?>' <?=$disabled;?>> 
+		<img src="assets/img/office-chair-left.png">
+	</label>
+<?php
+}
+?>
 </div>
 
 <div class="table-rect-dt"></div>
 
 <div class="right-chairs">
-
-<?php foreach ($derecha as $asiento) { ?>
-
-<label class="chair">
-
 <?php
-$disabled = in_array($asiento, $ass_dis) ? '' : 'disabled';
+foreach ($derecha as $asiento) {
 ?>
-
-<input type='radio' name='asiento' value='<?=$asiento;?>' <?=$disabled;?>>
-<img src="assets/img/office-chair-right.png">
-
-
-</label>
-
-<?php } ?>
-
+	<label class="chair">
+<?php
+		$disabled = in_array($asiento, $ass_dis) ? '' : 'disabled';
+?>
+		<input type='radio' name='asiento' value='<?=$asiento;?>' <?=$disabled;?>>
+		<img src="assets/img/office-chair-right.png">
+	</label>
+<?php
+}
+?>
 </div>
 </div>
 
@@ -135,7 +92,6 @@ $disabled = in_array($asiento, $ass_dis) ? '' : 'disabled';
 
 <input class="btn btn-primary shadow mb-3" type="submit" value="Reservar">
 </form>
-
 
 <a class="btn btn-primary" href="./?page=espacio_uno&fecha=<?=$fecha;?>">Volver a espacio Uno para el <?=$fecha;?></a>
 <a class="btn btn-primary" href="./?page=espacio_tres&fecha=<?=$fecha;?>">Ver en espacio Tres para el <?=$fecha;?></a>
--- phpfiles/espacio_tres.php
+++ phpfiles/espacio_tres.php
@@ -1,35 +1,19 @@
 
 <div class="container py-5">
+<h5 class="mb-3">Reservas - <a href="./">Home</a></h5>
+
 <?php
 
-function connect() {
-    $host = 'localhost';
-    $db = 'reservas';
-    $usuario = 'root';
-    $contrasena = '';
-
-    try {
-        $pdo = new PDO("mysql:host=$host;dbname=$db", $usuario, $contrasena);
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        return $pdo;
-    } catch (PDOException $e) {
-        echo "Error de conexión: " . $e->getMessage();
-        die();
-    }
-}
-
-$pdo = connect();
+$id = 1; // el id debe ser el del usuario logueado
 
 $disponible = range(17, 22);
-
 $izquierda = array_slice($disponible, 0, 3);
 $derecha = array_slice($disponible, 3);
 
 $fecha = $_GET['fecha'];
 
-$ass_dis = habilitados($fecha, $disponible, $pdo);
-
-$reservas = rango($fecha, $disponible, $pdo);
+$ass_dis = $app->habilitados($fecha, $disponible);
+$reservas = $app->rango($fecha, $disponible);
 
 ?>
 
@@ -38,55 +22,23 @@ $reservas = rango($fecha, $disponible, $pdo);
 <div class="shadow rounded-4 p-3">
 <h5 class="mb-3">Reservados para el d&iacute;a <?=$fecha;?></h5>
 
-
 <?php
+
 if (empty($reservas)) {
-    echo '<p class="mb-0">No hay lugares reservados para esta fecha.</p>';
+	echo '<p class="mb-0">No hay lugares reservados para esta fecha.</p>';
 } else {
-    foreach ($reservas as $reserva) {
+	foreach ($reservas as $reserva) {
 ?>
-<div class="d-flex justify-content-between align-items-center">
-<p class="text-capitalize mb-0"><?=$reserva['nombre'];?></p>
-Asiento #<?=$reserva['asiento'];?>
-</div>
-<hr>
+		<div class="d-flex justify-content-between align-items-center">
+			<p class="text-capitalize mb-0"><?=$reserva['nombre'];?></p>
+			Asiento #<?=$reserva['asiento'];?>
+		</div>
+		<hr>
 <?php
-    }
+	}
 }
 
-function rango($fecha, $disponible, $pdo){
-
-    $sql = "SELECT * FROM lista LEFT JOIN usuarios ON lista.idusuario = usuarios.id WHERE lista.fecha = :fecha AND lista.asiento IN (" . implode(',', $disponible) . ")";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':fecha', $fecha, PDO::PARAM_STR);
-    $stmt->execute();
-
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-
-function reservas_hoy($fecha, $pdo){
-
-    $sql = "SELECT * FROM lista LEFT JOIN usuarios ON lista.idusuario = usuarios.id WHERE lista.fecha = :fecha";
-    $stmt = $pdo->prepare($sql);
-    $stmt->bindParam(':fecha', $fecha, PDO::PARAM_STR);
-    $stmt->execute();
-
-    return $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-
-function habilitados($fecha, $disponible, $pdo)
-{
-     $reservas = reservas_hoy($fecha, $pdo);
-
-    $ass_res = array_column($reservas, 'asiento');
-
-    $ass_dis = array_diff($disponible, $ass_res);
-
-    return $ass_dis;
-}
-
-
-$ass_dis = habilitados($fecha, $disponible, $pdo);
+$ass_dis = $app->habilitados($fecha, $disponible);
 
 ?>
 
@@ -99,42 +51,37 @@ $ass_dis = habilitados($fecha, $disponible, $pdo);
 <h5 class="mb-3">Seleccione un asiento</h5>
 
 <form action="" method="post">
-
 <div class="tablet">
 <div class="left-chairs">
-<?php foreach ($izquierda as $asiento) { ?>
-<label class="chair" style="border-radius: 10px 0 0 10px;">
 <?php
-$disabled = in_array($asiento, $ass_dis) ? '' : 'disabled';
+foreach ($izquierda as $asiento) {
 ?>
-<input type='radio' name='asiento' value='<?=$asiento;?>' <?=$disabled;?>> 
-<img src="assets/img/office-chair-left.png">
-
-
-</label>
-<?php } ?>
+	<label class="chair" style="border-radius: 10px 0 0 10px;">
+<?php
+		$disabled = in_array($asiento, $ass_dis) ? '' : 'disabled';
+?>
+		<input type='radio' name='asiento' value='<?=$asiento;?>' <?=$disabled;?>> 
+		<img src="assets/img/office-chair-left.png">
+	</label>
+<?php
+}
+?>
 </div>
 
 <div class="table-rect-dt"></div>
 
 <div class="right-chairs">
-
-<?php foreach ($derecha as $asiento) { ?>
-
-<label class="chair">
-
 <?php
-$disabled = in_array($asiento, $ass_dis) ? '' : 'disabled';
+foreach ($derecha as $asiento) {
 ?>
-
-<input type='radio' name='asiento' value='<?=$asiento;?>' <?=$disabled;?>>
-<img src="assets/img/office-chair-right.png">
-
-
-</label>
-
+	<label class="chair">
+<?php
+		$disabled = in_array($asiento, $ass_dis) ? '' : 'disabled';
+?>
+		<input type='radio' name='asiento' value='<?=$asiento;?>' <?=$disabled;?>>
+		<img src="assets/img/office-chair-right.png">
+	</label>
 <?php } ?>
-
 </div>
 </div>
 
@@ -142,7 +89,6 @@ $disabled = in_array($asiento, $ass_dis) ? '' : 'disabled';
 <br>
 <input class="btn btn-primary shadow mb-3" type="submit" value="Reservar">
 </form>
-
 
 <a class="btn btn-primary" href="./?page=espacio_uno&fecha=<?=$fecha;?>">Volver a espacio Uno para el <?=$fecha;?></a>
 <a class="btn btn-primary" href="./?page=espacio_dos&fecha=<?=$fecha;?>">Ver en espacio Dos para el <?=$fecha;?></a>
